eb646dfc41a532cb771c939f6737c87b46806469
rutujamusale <rutujamusale846@gmai.com>
2025-08-10 11:18:44 +0530

    user entity updated.

--- Backend/src/main/java/com/bus_ticket/entities/User.java
+++ Backend/src/main/java/com/bus_ticket/entities/User.java
@@ -24,11 +24,20 @@ public class User extends BaseEntity {
     @Column(name = "password", nullable = false)
     private String password;
     
-    @Column(name = "phone_number")
-    private String phoneNumber;
+    @Column(name = "phone")
+    private String phone;
     
     @Column(name = "address")
     private String address;
+    
+    @Column(name = "city")
+    private String city;
+    
+    @Column(name = "state")
+    private String state;
+    
+    @Column(name = "pincode")
+    private String pincode;
     
     @Column(name = "is_deleted")
     private boolean isDeleted = false;
