3c073ba25f1daa8b6185490a2b86c415c4e4309a
sharbani008 <msharbani08@gmail.com>
2025-08-10 21:10:48 +0530

    adding booking, schedule, transaction, seat and related repositories

--- Backend/src/main/java/com/bus_ticket/dao/TransactionDao.java
+++ Backend/src/main/java/com/bus_ticket/dao/TransactionDao.java
@@ -1,14 +1,14 @@
 package com.bus_ticket.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
 import com.bus_ticket.entities.Transaction;
-
+import java.util.List;
 
 @Repository
 public interface TransactionDao extends JpaRepository<Transaction, Long> {
-    
-    
+    List<Transaction> findByUserId(Long userId);
+    List<Transaction> findByPaymentStatus(String paymentStatus);
+    List<Transaction> findByUserIdAndPaymentStatus(Long userId, String paymentStatus);
 }
